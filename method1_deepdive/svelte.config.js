e25fe8519fd10a2fb6847361ced99ff9b7d8f75d
lenntil <squibs.tykes0a@icloud.com>
2023-12-05 12:59:25 +0900

    Update base tag

--- method1_deepdive/svelte.config.js
+++ method1_deepdive/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+      base: process.env.PUBLIC_BASE_PATH,
     },
   },
 };
